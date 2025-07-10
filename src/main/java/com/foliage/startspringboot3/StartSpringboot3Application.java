3c66f77cc42c8ed0f3cbdc15f206dfe171f7870c
GodLy <14707541+hoogodly@user.noreply.gitee.com>
2025-07-10 13:13:15 +0800

    1.创建项目

---
+++ src/main/java/com/foliage/startspringboot3/StartSpringboot3Application.java
@@ -0,0 +1,13 @@
+package com.foliage.startspringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartSpringboot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartSpringboot3Application.class, args);
+    }
+
+}
