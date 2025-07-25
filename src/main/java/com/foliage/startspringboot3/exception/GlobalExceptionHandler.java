1a6a08035c6d84b3164900398fd47d17f1240ce8
LittleK <2176054855@qq.com>
2025-07-25 14:44:44 +0800

    Update GlobalExceptionHandler.java

--- src/main/java/com/foliage/startspringboot3/exception/GlobalExceptionHandler.java
+++ src/main/java/com/foliage/startspringboot3/exception/GlobalExceptionHandler.java
@@ -10,11 +10,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 @Hidden
 public class GlobalExceptionHandler {
+    @ExceptionHandler(BusinessException.class)
     public BaseResponse<?> businessExceptionHandler(BusinessException e) {
         log.error("BusinessException", e);
         return ResultUtils.error(e.getCode(), e.getMessage());
     }
-
+    @ExceptionHandler(RuntimeException.class)
     public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
         log.error("RuntimeException", e);
         return ResultUtils.error(ErrorCode.SYSTEM_ERROR);
